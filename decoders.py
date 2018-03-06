406744fa33daff0960ab809d989b5b3aefb7fe8d
YoungJoon Chun <yjchun@mac.com>
2018-03-06 19:08:43 +0900

    minimally working initial version of transducer decoder.

--- decoders.py
+++ decoders.py
@@ -6,41 +6,47 @@ def log_sum_exp(a, b):
     """
     return max(a, b) + math.log1p(math.exp(-abs(a-b)))
 
+
+#TODO: it works but not passing test
 def decode_static(log_probs, beam_size=1, blank=0):
     """
-    Decode best prefix in the RNN Transducer.  This decoder is static, it does
-    not update the next step distribution based on the previous prediction. As
-    such it looks for hypotheses which are length U.
+    :param log_probs: FloatTensor(T,U,V) where T=encoder output, U=Predicted output, V=nbclasses
     """
     T, U, V = log_probs.shape
-    beam = [((), 0)];
-    for i in range(T + U - 2):
-        new_beam = {}
-        for hyp, score in beam:
-            u = len(hyp)
-            t = i - u
-            for v in range(V):
-                if v == blank:
-                    if t < T - 1:
+    beam = [[(), 1.0]] # list of best k so far.
+
+    t = 0
+    for u in range(U):
+        while t < T:
+            new_beam = {}
+            # Pr(k|u,t)
+            for hyp, score in beam:
+                # find all probability
+                for v in range(V):
+                    if v == blank:
                         new_hyp = hyp
                         new_score = score + log_probs[t, u, v]
-                elif u < U - 1:
-                    new_hyp = hyp + (v,)
-                    new_score = score + log_probs[t, u, v]
-                else:
-                    continue
+                    else:
+                        new_hyp = hyp + (v,)
+                        new_score = score + log_probs[t, u, v]
 
-                old_score = new_beam.get(new_hyp, None)
-                if old_score is not None:
-                    new_beam[new_hyp] = log_sum_exp(old_score, new_score)
-                else:
-                    new_beam[new_hyp] = new_score
+                    old_score = new_beam.get(new_hyp, None)
+                    if old_score is not None:
+                        new_beam[new_hyp] = log_sum_exp(old_score, new_score)
+                    else:
+                        new_beam[new_hyp] = new_score
 
-        new_beam = sorted(new_beam.items(), key=lambda x: x[1], reverse=True)
-        beam = new_beam[:beam_size]
+            new_beam = sorted(new_beam.items(), key=lambda x: x[1], reverse=True)
+            beam = new_beam[:beam_size]
+            maxlen = max([len(l[0]) for l in beam])
+            if maxlen == u: # BLANK. advance T
+                t += 1
+                continue
+            break
 
     hyp, score = beam[0]
     return hyp, score + log_probs[-1, -1, blank]
+
 
 if __name__ == "__main__":
     import transducer.ref_transduce as rt
@@ -54,6 +60,7 @@ if __name__ == "__main__":
     log_probs = np.random.randn(T, U, V)
     log_probs = rt.log_softmax(log_probs, axis=2)
     labels, beam_ll = decode_static(log_probs, beam_size, blank)
+    print(labels)
     _, ll = rt.forward_pass(log_probs, labels, blank)
     assert np.allclose(ll, beam_ll, rtol=1e-9, atol=1e-9), \
             "Bad result from beam search."
